a076e2d14e1b51d4caa4b5171c38120498d50fdd
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2023-07-04 22:48:56 +0900

    [Silver III] Title: 수열, Time: 76 ms, Memory: 40292 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -6,17 +6,11 @@ n,k = map(int,input().split())
 
 a = list(map(int,input().split()))
 
-b = []
-cnt = 0
-for i in range(n-k+1):
-  if i == 0:
-    for j in range(k):
-      cnt += a[j]
-    b.append(cnt)
-    cnt -= a[i]
-  else:
-    cnt += a[i+k-1]
-    b.append(cnt)
-    cnt -= a[i]
+re = []
 
-print(max(b))
+re.append(sum(a[:k]))
+
+for i in range(n-k):
+  re.append(re[i] - a[i] + a[k+i])
+
+print(max(re))
