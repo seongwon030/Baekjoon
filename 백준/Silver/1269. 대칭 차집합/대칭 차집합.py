3ed4c367f72398e8b7bcbe94364db7b8cd073572
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2023-07-13 17:41:54 +0900

    [Silver IV] Title: 대칭 차집합, Time: 364 ms, Memory: 114236 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -0,0 +1,15 @@
+n,m = map(int,input().split())
+
+a = set()
+b = set()
+
+k = list(map(int,input().split()))
+k2 = list(map(int,input().split()))
+
+for i in range(len(k)):
+  a.add(k[i])
+for j in range(len(k2)):
+  b.add(k2[j])
+
+re = (a-(a&b)) | (b-(a&b))
+print(len(re))
