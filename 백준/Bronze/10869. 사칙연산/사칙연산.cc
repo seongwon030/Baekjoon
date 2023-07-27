4378ea081435809364b64a475b233e392539fa68
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2023-07-27 14:32:06 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void) {
+	int a;
+	int b;
+	std::cin >> a >> b;
+	std::cout << a + b << std::endl;
+	std::cout << a - b << std::endl;
+	std::cout << a*b << std::endl;
+	std::cout << a/b << std::endl;
+	std::cout << a % b;
+	return  0;
+} 
