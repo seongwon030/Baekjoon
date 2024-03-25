157bbd9aba663c8b488224d08af3075d7b601286
서성원 <seongwon0903@gmail.com>
2024-03-25 14:36:35 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 136 ms, Memory: 35200 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+result, cnt = 0,0
+MAX = 10**5 # 움직일 수 있는 최대좌표
+dist = [-1] * (MAX + 1) # 해당 위치에 도착했을 때 시간
+n, k = map(int,input().split())
+
+q = deque()
+q.append(n)
+dist[n] = 0
+while q:
+    x = q.popleft()
+    if x==k: # x변수인 수빈의 위치가 동생이 있는 k와 같을 때 멈춤
+      result = dist[x]
+      break
+    # nx = 4,6,10 (현재 위치 5일 때 이동할 수 있는 방향)
+    for nx in (x*2, x-1, x+1):
+      # 범위내에 있고 아직 방문안했거나 이전 방문 + 1이 현재
+      if 0<=nx<=MAX and dist[nx] == -1:
+        if nx == x*2:
+          dist[nx] = dist[x]
+        else:
+          dist[nx] = dist[x] + 1
+        q.append(nx)
+print(result)
