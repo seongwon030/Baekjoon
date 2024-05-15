ae54a0f7cd1a2b6d26b31c9b211d0cd66b35d6a0
서성원 <seongwon0903@gmail.com>
2024-05-15 15:21:39 +0900

    [Gold V] Title: 암호 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -3,19 +3,14 @@ from itertools import combinations
 L,C = map(int,input().split())
 
 k = list(map(str,input().split()))
+k.sort()
+
 vowels = 'aeiou'
 
-vowel_cnt = 0
-consonant_cnt = 0
+com = combinations(k,L)
 
-result = []
-for c in combinations(k,L):
-  vowel_str = ''.join(sorted(c))
-  vowel_cnt = sum(1 for char in vowel_str if char in vowels)
+for c in com:
+  vowel_cnt = sum(1 for char in c if char in vowels)
   consonant_cnt = L - vowel_cnt
-  if vowel_cnt >=1 and consonant_cnt >=2:
-    result.append(vowel_str)
-
-result.sort()
-for word in result:
-    print(word)
+  if vowel_cnt>=1 and consonant_cnt>=2:
+    print(''.join(c))
