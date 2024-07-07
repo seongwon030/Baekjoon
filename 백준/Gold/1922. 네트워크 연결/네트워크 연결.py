6b6d640bc1754a25fed4dc8a80f0567ef2fc7f83
서성원 <seongwon0903@gmail.com>
2024-07-07 14:45:57 +0900

    [Gold IV] Title: 네트워크 연결, Time: 4148 ms, Memory: 53948 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,29 @@
+# 네트워크 연결 - 골드4
+
+n = int(input()) # 정점의 개수
+m = int(input()) # 간선의 개수
+root = [i for i in range(n+1)]
+edge = []
+
+for i in range(m):
+  edge.append(list(map(int,input().split())))
+
+edge.sort(key=lambda x:x[2])
+
+def find(x):
+  if x!=root[x]:
+    root[x] = find(root[x])
+  return root[x]
+
+ans = 0
+for a,b,c in edge:
+  aRoot = find(a)
+  bRoot = find(b)
+  if aRoot != bRoot:
+    if aRoot > bRoot:
+      root[aRoot] = bRoot
+    else:
+      root[bRoot] = aRoot
+    ans += c
+
+print(ans)
