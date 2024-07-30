fa06bf71161d0e62b6789a6f26c48a509185b687
서성원 <seongwon0903@gmail.com>
2024-07-30 23:15:19 +0900

    [Gold IV] Title: 알파벳, Time: 1364 ms, Memory: 52904 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,23 +1,25 @@
+# 수정 후
+from collections import deque
+
+def bfs(original, x, y):
+    max_len = 1
+    q = set([(x, y, original[y][x])])  
+    while q:
+        x, y, alpha = q.pop()  
+        max_len = max(max_len, len(alpha))
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < c and 0 <= ny < r and original[ny][nx] not in alpha:
+                q.add((nx, ny, alpha + original[ny][nx]))  
+    return max_len
+
 r, c = map(int, input().split())
+original = []
+for _ in range(r):
+    original.append(list(input()))
 
-k = [list(input()) for _ in range(r)]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-ans = 0
-alpha = set()
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def dfs(x, y, count):
-    global ans
-    ans = max(ans, count)
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < r and 0 <= ny < c and not k[nx][ny] in alpha:
-            alpha.add(k[nx][ny])
-            dfs(nx, ny, count+1)
-            alpha.remove(k[nx][ny])
-
-alpha.add(k[0][0])
-dfs(0, 0, 1)
-print(ans)
+print(bfs(original, 0, 0))
