11c482692db85470d1fa80e5141512f3c6ba0b83
seongwon seo <seongwon0903@gmail.com>
2025-04-10 00:43:24 +0900

    [Platinum IV] Title: Convex Hull, Time: 520 ms, Memory: 47244 KB -BaekjoonHub

---
+++ 백준/Platinum/4181. Convex Hull/Convex Hull.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+def ccw(p1, p2, p3):
+    return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
+
+def convex(points):
+  points = sorted(points)
+
+  lower = []
+  for p in points:
+    while len(lower) >= 2 and ccw(lower[-2], lower[-1], p) < 0:
+      lower.pop()
+    lower.append(p)
+  
+  upper = []
+  for p in reversed(points):
+    while len(upper) >= 2 and ccw(upper[-2], upper[-1], p) < 0:
+      upper.pop()
+    upper.append(p)
+
+  return lower[:-1] + upper[:-1]
+
+
+n = int(input())
+
+points = []
+for _ in range(n):
+  x,y,c = input().split()
+  if c == 'Y':
+    points.append([int(x),int(y)])
+
+print(len(convex(points)))
+
+for p in convex(points):
+  print(p[0],p[1])
