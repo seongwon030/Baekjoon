d52bb89eaddd853974eaf9b6e192bdebfaa1b9f9
seongwon seo <seongwon0903@gmail.com>
2025-05-18 18:40:46 +0900

    [Silver II] Title: 유기농 배추, Time: 48 ms, Memory: 32808 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,16 +1,13 @@
 import sys
-
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
-
-T = int(input())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def go(x,y, visited):
     global count
-    if x < 0 or x >= N or y < 0 or y >= M or visited[x][y] or graph[x][y] == 0:
+    if x < 0 or x >= n or y < 0 or y >= m or visited[x][y] or graph[x][y] == 0:
         return
 
     visited[x][y] = True
@@ -23,21 +20,23 @@ def go(x,y, visited):
 
     return count
 
-for i in range(T):
-    M, N, K = map(int, input().split())
-    graph = [[0] * M for _ in range(N)]
-    visited = [[False] * M for _ in range(N)]
+T = int(input())
 
-    for _ in range(K):
+for i in range(T):
+    m,n,k = map(int,input().split())
+    graph = [[0] * m for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]
+
+    for _ in range(k):
         a,b = map(int, input().split())
         graph[b][a] = 1
 
     all_counts = []
-    for x in range(N):
-        for y in range(M):
+    for x in range(n):
+        for y in range(m):
             if graph[x][y] == 1 and not visited[x][y]:
                 count = 0
-                go(x, y, visited)
+                go(x,y,visited)
                 all_counts.append(count)
 
     print(len(all_counts))
