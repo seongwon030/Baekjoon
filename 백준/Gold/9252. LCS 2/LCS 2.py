d205a24462398a2c88876c6e31793f9b639b87f6
seongwon seo <seongwon0903@gmail.com>
2025-06-17 19:55:45 +0900

    [Gold IV] Title: LCS 2, Time: 428 ms, Memory: 56604 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -4,18 +4,28 @@ input = sys.stdin.readline
 m = ' ' + input().rstrip()
 n = ' ' + input().rstrip()
 
-string_dp = [[''] * len(n) for _ in range(len(m))]
+len_dp = [[0] * len(n) for _ in range(len(m))]
 
 for i in range(1, len(m)):
     for j in range(1, len(n)):
         if m[i] == n[j]:
-            string_dp[i][j] = string_dp[i-1][j-1] + m[i]
+            len_dp[i][j] = len_dp[i-1][j-1] + 1
         else:
-            if len(string_dp[i][j-1]) > len(string_dp[i-1][j]):
-                string_dp[i][j] = string_dp[i][j-1]
-            else:
-                string_dp[i][j] = string_dp[i-1][j]
+            len_dp[i][j] = max(len_dp[i-1][j], len_dp[i][j-1])
 
-print(len(string_dp[-1][-1]))
-if len(string_dp[-1][-1]) > 0:
-    print(string_dp[-1][-1])
+i,j = len(m) - 1, len(n) -1
+lcs = []
+
+while i > 0 and j > 0:
+    if m[i] == n[j]:
+        lcs.append(m[i])
+        i -= 1
+        j -= 1
+    elif len_dp[i-1][j] > len_dp[i][j-1]:
+        i -= 1
+    else:
+        j -= 1
+
+print(len(lcs))
+if lcs:
+    print(''.join(reversed(lcs)))
